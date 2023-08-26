95e7d262c6edb045d09e0cf94ca60ce52e3a0c7a
olhkyle <kylekwon96@gmail.com>
2023-08-26 23:13:58 +0900

    [STYLE] update Category Tag style

--- src/components/common/CategoryTag.js
+++ src/components/common/CategoryTag.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const CategoryTag = ({ category }) => {
-	return <span className="px-4 py-2 rounded-2xl border-2 border-blue-primary font-semibold text-blue-primary bg-blue-primary/40">{category}</span>;
+	return <span className="px-2 py-1 rounded-2xl border-2 border-blue-primary font-semibold text-blue-primary bg-blue-primary/40">{category}</span>;
 };
 
 export default CategoryTag;
